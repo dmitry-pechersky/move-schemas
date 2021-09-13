5789a8e6bbcaf260c046ce7ef36a7f79571f21dc
None <@none@none.com>
2021-09-13 17:49:17 +0300

    DP Export

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,15 +3,16 @@ pipeline {
     environment {
         EXP_HOST = 'linux02'
         EXP_SERVICE_NAME = 'db01'
-        EXP_SCHEMAS = 'SIGMA'
+        EXP_SCHEMAS = 'SIGMA,DBCCB'
         EXP_CREDENTIALS = credentials('oracle-rdbms-linux02-db01-jenkins-prod-id')
+        EXP_DIRECTORY = 'DATA_PUMP_DIR'
+        
     }    
     stages {
         stage('Export') {
             steps {
-                sh('$PYTHON_INTERPRETER ./python/export_schemas.py ${EXP_CREDENTIALS_USR} ${EXP_CREDENTIALS_PSW}')
+                sh('$PYTHON_INTERPRETER ./python/export_schemas.py')
             }
         }
     }
 }
-
--- python/export_schemas.py
+++ python/export_schemas.py
@@ -1,6 +1,8 @@
 import os
 import cx_Oracle
 import sys
+import subprocess
+import time
 
 class MSException(Exception):
     pass
@@ -15,20 +17,26 @@ class ODB:
             res_set = cursor.execute("select count(*) cnt from dba_users where username = :user_name", user_name=user).fetchone()
             return res_set[0] == 1
 
+def rdbms_export(host, service_name, user, password, schemas, directory):
+    subprocess.run(["expdp", f'{user}@"{host}/{service_name}"', f"SCHEMAS={schemas}", "COMPRESSION=ALL", f"DIRECTORY={directory}"],
+                   check=True, text=True, 
+                   input=f"{password}\n")
+
 if __name__ == '__main__':
-    for i in os.environ['EXP_CREDENTIALS_USR']:
-        print("C" + i)
-    for i in os.environ['EXP_CREDENTIALS_PSW']:
-        print("C" + i)
-    """
-    source_db = ODB(host=os.environ["MS_SOURCE_HOST"], service_name=os.environ["MS_SOURCE_SERVICE_NAME"], user='jenkins', password='hdd8d83ddef34')
+    exp_host = os.environ["EXP_HOST"]
+    exp_service_name = os.environ["EXP_SERVICE_NAME"]
+    exp_user = os.environ['EXP_CREDENTIALS_USR']
+    exp_password = os.environ['EXP_CREDENTIALS_PSW']
+    exp_schemas = os.environ['EXP_SCHEMAS']
+    exp_directory = os.environ['EXP_DIRECTORY']
+
     """
     source_db = ODB(host=os.environ["EXP_HOST"], 
                     service_name=os.environ["EXP_SERVICE_NAME"], 
                     user=os.environ['EXP_CREDENTIALS_USR'], 
                     password=os.environ['EXP_CREDENTIALS_PSW'])
-    """
-
     if not source_db.user_exists(schema):
         raise MSException(f"User {schema} doesn't exist")
     """
+    rdbms_export(host=exp_host, service_name=exp_service_name, user=exp_user, password=exp_password, 
+                 schemas=exp_schemas, directory=exp_directory)
